1899156460b16cd625b3cd113669b37f07f2228b
Ravi Jain <34270478+ravijainpro@users.noreply.github.com>
2019-11-08 11:14:32 +0530

    Update writer.c

--- ipc/writer.c
+++ ipc/writer.c
@@ -19,7 +19,14 @@ int main()
 
 	// msgget creates a message queue 
 	// and returns identifier 
+	/*
+	Processes requesting access to an IPC facility must be able to identify it. 
+	To do this, functions that initialize or provide access to an IPC facility use a key_t key argument. 
+	(key_t is essentially an int type defined in <sys/types.h> 
+	*/
 	msgid = msgget(key, 0666 | IPC_CREAT); 
+	//When the IPC_CREAT flag is supplied in the flags argument appropriate to the call, the function tries to create the facility if it does not exist already. 
+	//The Message queue is created with a basic key and message flag msgid = IPC_CREAT | 0666 -- create queue and make it read and appendable by all. 
 	message.mesg_type = 1; 
 
 	printf("Write Data : "); 
