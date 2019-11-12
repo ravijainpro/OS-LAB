fbe78a8d654c7c0484a165a49ce9d6adb09d65f9
Ravi Jain <34270478+ravijainpro@users.noreply.github.com>
2019-11-12 23:31:41 +0530

    Update best_fit.cpp

--- memory management/best_fit.cpp
+++ memory management/best_fit.cpp
@@ -1,3 +1,23 @@
+/* ================SOMETHING WORTH A READ=============
+Is Best-Fit really best?
+Although, best fit minimizes the wastage space, it consumes a lot of processor time for searching the block which is close to required size. Also, Best-fit may perform poorer than other algorithms in some cases. For example, see below exercise.
+
+Example: Consider the requests from processes in given order 300K, 25K, 125K and 50K. Let there be two blocks of memory available of size 150K followed by a block size 350K.
+
+Best Fit:
+300K is allocated from block of size 350K. 50 is left in the block.
+25K is allocated from the remaining 50K block. 25K is left in the block.
+125K is allocated from 150 K block. 25K is left in this block also.
+50K can’t be allocated even if there is 25K + 25K space available.
+
+First Fit:
+300K request is allocated from 350K block, 50K is left out.
+25K is be allocated from 150K block, 125K is left out.
+Then 125K and 50K are allocated to remaining left out partitions.
+So, first fit can handle requests.
+
+*/
+
 // C++ implementation of Best - Fit algorithm 
 #include<bits/stdc++.h> 
 using namespace std; 
